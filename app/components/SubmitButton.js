9c03e63a5ae06923da1be8f0efa5452979e60877
Kishore <abkishoreg@gmail.com>
2022-01-11 12:24:38 +0530

    sixth commit ~ Form building using FORMIK and YUP. 'AppForm' reusable component was created to avoid ugly syntax for formik tag in loginscreen and as well as the SubmitButton

---
+++ app/components/SubmitButton.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { useFormikContext } from "formik";
+
+import AppButton from "./AppButton";
+import { colors } from "../config/colors";
+
+function SubmitButton({ children }) {
+  const { handleSubmit } = useFormikContext();
+  return (
+    <AppButton onPress={handleSubmit} style={styles.button}>
+      {children}
+    </AppButton>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 30,
+    backgroundColor: colors.secondary,
+    alignSelf: "center",
+  },
+});
+
+export default SubmitButton;
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { StyleSheet, Image } from "react-native";
-import { Formik } from "formik";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
-import AppTextInput from "../components/AppTextInput";
-import AppButton from "../components/AppButton";
-import AppText from "../components/AppText";
 import AppFormField from "../components/AppFormField";
-
-import { colors } from "../config/colors";
+import AppForm from "../components/AppForm";
+import SubmitButton from "../components/SubmitButton";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -20,49 +16,44 @@ function LoginScreen(props) {
   return (
     <Screen style={styles.container}>
       <Image source={require("../assets/logo2.gif")} style={styles.logo} />
-      <Formik
+
+      <AppForm
         initialValues={{ email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {({ handleSubmit }) => (
-          <>
-            <AppFormField
-              autoCaptilize="none"
-              autoCorrect={false}
-              icon="email"
-              name="email"
-              keyboardType="email-address"
-              placeholder="Enter email"
-              style={styles.input}
-            />
+        <AppFormField
+          autoCaptilize="none"
+          autoCorrect={false}
+          icon="email"
+          name="email"
+          keyboardType="email-address"
+          placeholder="Enter email"
+          style={styles.input}
+        />
 
-            {/* {touched.email && errors.email && (
+        {/* {touched.email && errors.email && (
               <AppText style={{ color: colors.danger }}>{errors.email}</AppText>
             )} */}
 
-            <AppFormField
-              autoCaptilize="none"
-              autoCorrect={false}
-              icon="lock"
-              name="password"
-              placeholder="Enter password"
-              secureTextEntry
-              style={styles.input}
-            />
+        <AppFormField
+          autoCaptilize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Enter password"
+          secureTextEntry
+          style={styles.input}
+        />
 
-            {/* {touched.password && errors.password && (
+        {/* {touched.password && errors.password && (
               <AppText style={{ color: colors.danger }}>
                 {errors.password}
               </AppText>
             )} */}
 
-            <AppButton onPress={handleSubmit} style={styles.button}>
-              Login
-            </AppButton>
-          </>
-        )}
-      </Formik>
+        <SubmitButton>Login</SubmitButton>
+      </AppForm>
     </Screen>
   );
 }
@@ -79,11 +70,6 @@ const styles = StyleSheet.create({
   },
   input: {
     flex: 1,
-  },
-  button: {
-    marginTop: 30,
-    backgroundColor: colors.secondary,
-    alignSelf: "center",
   },
 });
 
